fe5e538d9d06e2fec5f723d26b541a68372cef48
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-10-25 22:38:39 +0530

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter App"),
+      locale: DevicePreview.of(context).locale, // <--- /!\ Add the locale
+      builder: DevicePreview.appBuilder, // <--- /!\ Add the builder
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
         ),
-        body: Center(
-          child: Text(
-            "Hello World!",
-            style: TextStyle(fontSize: 20.0),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
           ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+        flexibleSpace: Image.asset(
+          "assests/background.jpg",
+          fit: BoxFit.cover,
         ),
       ),
     );
